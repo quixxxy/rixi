0b1a0f834b3f4a87a8d54274016e149bfdf40e33
Sergii Iakymenko1 <v-siakymenko1@expedia.com>
2016-03-29 17:30:51 -0700

    split into views

--- rixi-ui/src/main/resources/static/js/main.js
+++ rixi-ui/src/main/resources/static/js/main.js
@@ -1,8 +1,45 @@
-app = angular.module('rixi', []);
+app = angular.module('rixi-app', ['ngRoute']);
 
-app.controller('UserCtrl', function ($scope, $http) {
+app.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider
+            .when('/',
+            {
+                controller: 'UsersCtrl',
+                templateUrl: 'view/users.html'
+            })
+            .when('/user/:userId',
+            {
+                controller: 'UserCtrl',
+                templateUrl: 'view/user.html'
+            })
+            .when('/contact',
+            {
+                templateUrl: 'view/contact.html'
+            })
+            .when('/about',
+            {
+                templateUrl: 'view/about.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }]);
+
+app.controller('UsersCtrl', function ($scope, $http) {
     $http.get("http://localhost:8081/user")
         .success(function (response) {
             $scope.users = response;
         });
+});
+
+app.controller('UserCtrl', function ($scope, $http, $routeParams) {
+    $http.get("http://localhost:8081/user/" + $routeParams.userId)
+        .success(function (response) {
+            $scope.user = response;
+        });
+
+    $scope.update = function () {
+        console.log("123");
+    };
 });
